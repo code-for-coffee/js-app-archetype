f8131b4576d6ce77075e70171615fa8f139c1edd
James Traver <james.traver@backstopsolutions.com>
2018-12-31 14:46:30 -0600

    test workshop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,75 @@
-const test = () => {
-  return 'Test!';
-};
+class Party {
+  constructor() {
+    // when a new Party() happens,
+    // this happens first
+    this.people = new Array();
+    this.snacks = new Array();
+    this.beverages = new Array();
+    this.activities = new Array();
+  }
 
-test();
+  /**
+   * invite someone to the party
+   * @param {*} name
+   */
+  invitePerson(name) {
+    this.people.push(name);
+  }
+
+  /**
+   * show who is at the party
+   */
+  getPeople() {
+    return this.people;
+  }
+
+  /**
+   * bring drinks to the party
+   * @param {*} drink
+   */
+  bringDrink(drink) {
+    this.beverages.push(drink);
+  }
+
+  /**
+   * show what drinks are available
+   */
+  listDrinks() {
+    return this.beverages;
+  }
+
+  /**
+   * bring stuff
+   * @param {*} snack
+   */
+  bringSnacks(snack) {
+    this.snacks.push(snack);
+  }
+
+  /**
+   * list the snacks
+   */
+  listSnacks() {
+    return this.snacks;
+  }
+
+  makeActivity(thingToDo) {
+    this.activities.push(thingToDo);
+  }
+
+  listActivities() {
+    return this.activities;
+  }
+
+  toString() {
+    // report the contents of the party
+    return `This Party contains: 
+    People: ${this.people.toString()} 
+    Snacks: ${this.snacks.toString()} 
+    Beverages: ${this.beverages.toString()} 
+    Activities: ${this.activities.toString()} 
+    `;
+  }
+}
+
+export default Party;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,2 +1,74 @@
 console.log('Hello there, General Kenobi');
 console.assert(42, true);
+
+import Party from '../src/index';
+import chai from 'chai';
+
+const expect = chai.expect;
+const should = chai.should();
+
+// a suite of tests
+describe('a party', () => {
+  it('can be instantiated', () => {
+    let myParty = new Party();
+    expect(typeof myParty).to.equal('object');
+    should.exist(myParty);
+  });
+
+  it('toString method returns a string', () => {
+    let myParty = new Party();
+    expect(typeof myParty.toString()).to.equal('string');
+  });
+});
+
+describe('a suite of tests in TDD methodology', () => {
+  // write tests first
+  // then make the tests pass
+  // repeat
+  // usually during pairing one writes test
+  // the other makes it pass
+  // rinse/repeat
+  it('can add people to the party', () => {
+    // 1. instantiate (Create) a new Party();
+    let myParty = new Party();
+    // we add something
+    myParty.invitePerson('Ryan');
+    // list what we added
+    let attendees = myParty.getPeople();
+    // confirm what we added was actually added
+    expect(attendees).to.deep.equal(['Ryan']);
+  });
+
+  it('can bring beverages to the party', () => {
+    // 1. instantiate (Create) a new Party();
+    let myParty = new Party();
+    // we add something
+    myParty.bringDrink('cherry coke');
+    // list what we added
+    let drinks = myParty.listDrinks();
+    // confirm what we added was actually added
+    expect(drinks).to.deep.equal(['cherry coke']);
+  });
+
+  it('can bring snacks to the party', () => {
+    // 1. instantiate (Create) a new Party();
+    let myParty = new Party();
+    // we add something
+    myParty.bringSnacks('cheetohs');
+    // list what we added
+    let snacks = myParty.listSnacks();
+    // confirm what we added was actually added
+    expect(snacks).to.deep.equal(['cheetohs']);
+  });
+
+  it('can have things to do at the party', () => {
+    // 1. instantiate (Create) a new Party();
+    let myParty = new Party();
+    // we add something
+    myParty.makeActivity('ticket to ride');
+    // list what we added
+    let activities = myParty.listActivities();
+    // confirm what we added was actually added
+    expect(activities).to.deep.equal(['ticket to ride']);
+  });
+});
